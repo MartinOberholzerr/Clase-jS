a0c2f906cd36c1377ec30c678479a0433a94ff77
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2022-11-14 15:26:01 -0300

    Haciendo que el boton se mueva hasta que no tenga datos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-let usuario =document.getElementById('#datos')
-let contrasena =document.getElementById('#contrasena')
-let boton =document.getElementById('#boton')
+let usuario = document.getElementById('#datos');
+let contrasena = document.getElementById('#contrasena');
+let boton = document.getElementById('#boton');
+    cont=0;
 
+    usuario.addEventListener = () =>{
+        boton.classList.add(`body-move`);
+    };
+    
