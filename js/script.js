ceb7d55a8d7057ee662f1e2d7cb87786c00ab98b
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2022-11-14 16:02:39 -0300

    haciendo que funcione

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-let usuario = document.getElementById('#datos');
-let contrasena = document.getElementById('#contrasena');
-let boton = document.getElementById('#boton');
-    cont=0;
-    
-    function inicio(){
-        document.getElementById("datos").addEventListener('click',usuario);
-    }
-
-    function usuario(){
-        let body = document.activeElement.id;
-        body.classList.remove(`active`);
+var datos =document.getElementById('datos')
+var contrasena =document.getElementById('contrasena')
+var boton =document.getElementById('boton')
+    cont=0
+datos.addEventListener('datos', stop)//datos en el input activa el efecto
+contrasena.addEventListener('datos', stop)
+    function stop(){
+        if(cont==0){
+            boton.classList.remove('body-move')//remueve la clase
+            cont++
+        }
+        else{
+            
+            boton.classList.add('input-Clave')//añade la clase 
+            cont--
+        }
     }
